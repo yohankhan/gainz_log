fb42ce13fb5d3502acfeadcc7eb9979e08d11ee3
yohan <yohank786@gmail.com>
2020-01-12 18:30:32 +0530

    frontend edit-3

--- screens/Setup.js
+++ screens/Setup.js
@@ -13,20 +13,70 @@ class Setup extends Component {
 		return (
 			<ImageBackground source={bg} style={{ width: '100%', height: '100%' }} resizeMode="cover">
 				<View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-					<View style={{ flex: 1, flexDirection: 'row', padding: 20 }}>
-						<View style={{ backgroundColor: 'blue', height: 50, width: 50 }}>
+					<View style={{ flex: 1, flexDirection: 'row' }}>
+						<View
+							style={{
+								width: 130,
+								height: 80,
+								elevation: 10,
+								backgroundColor: 'white',
+								marginHorizontal: 5,
+								marginVertical: 8,
+								borderRadius: 10
+							}}
+						>
 							<Text style={{ color: 'white' }}>DAY</Text>
 						</View>
-						<View style={{ backgroundColor: 'blue', height: 50, width: 50 }}>
+						<View
+							style={{
+								width: 130,
+								height: 80,
+								elevation: 10,
+								backgroundColor: 'white',
+								marginHorizontal: 5,
+								marginVertical: 8,
+								borderRadius: 10
+							}}
+						>
 							<Text style={{ color: 'white' }}>DAY 2</Text>
 						</View>
 					</View>
 
-					<View style={{ flex: 1, flexDirection: 'row', padding: 20 }}>
-						<View style={{ backgroundColor: 'blue', height: 50, width: 50 }}>
+					<View
+						style={{
+							width: 130,
+							height: 80,
+							elevation: 10,
+							backgroundColor: 'white',
+							marginHorizontal: 5,
+							marginVertical: 8,
+							borderRadius: 10
+						}}
+					>
+						<View
+							style={{
+								width: 130,
+								height: 80,
+								elevation: 10,
+								backgroundColor: 'white',
+								marginHorizontal: 5,
+								marginVertical: 8,
+								borderRadius: 10
+							}}
+						>
 							<Text style={{ color: 'white' }}>DAY 3</Text>
 						</View>
-						<View style={{ backgroundColor: 'blue', height: 50, width: 50 }}>
+						<View
+							style={{
+								width: 130,
+								height: 80,
+								elevation: 10,
+								backgroundColor: 'white',
+								marginHorizontal: 5,
+								marginVertical: 8,
+								borderRadius: 10
+							}}
+						>
 							<Text style={{ color: 'white' }}>DAY 4</Text>
 						</View>
 					</View>
